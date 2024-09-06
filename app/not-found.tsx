478953c480e335b5b8043b4cd5c241181abc5037
aayush-karna <ayushkarn32@gmail.com>
2024-09-06 23:41:21 +0545

    fixes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
     <div className="h-screen w-full flex flex-col justify-center items-center font-[family-name:var(--font-geist-mono)]">
       <h1>404 - Not Found</h1>
       <h2 className="mt-4">
-        Uh-oh! We lost this page in the void. Let's get you 127.0.0.1 !
+        Uh-oh! We lost this page in the void. Let&apos;s get you 127.0.0.1 !
       </h2>
     </div>
   );
