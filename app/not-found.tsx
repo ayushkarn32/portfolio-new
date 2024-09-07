e9877b808490586063b2621716e0921dc8227b48
aayush-karna <ayushkarn32@gmail.com>
2024-09-07 20:02:26 +0545

    updates

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center font-[family-name:var(--font-geist-mono)]">
+    <div className="h-screen p-4 w-full flex flex-col justify-center items-center font-[family-name:var(--font-geist-mono)]">
       <h1>404 - Not Found</h1>
       <h2 className="mt-4">
         Uh-oh! We lost this page in the void. Let&apos;s get you 127.0.0.1 !
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen text-lg w-full flex justify-center items-center font-[family-name:var(--font-geist-sans)]">
+    <div className="h-screen text-lg w-full flex justify-center items-center font-[family-name:var(--font-geist-sans)] dark:bg-[#2c3e50] dark:text-white transition-all ease-in-out delay-100">
       Hello World
     </div>
   );
